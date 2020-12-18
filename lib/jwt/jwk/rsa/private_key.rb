f4fb41bd86d0308c0af4164c6c5f8c7a3cb4533a
Joakim Antman <antmanj@gmail.com>
2020-12-18 22:34:10 +0200

    Beginning to split public an private keys

---
+++ lib/jwt/jwk/rsa/private_key.rb
@@ -0,0 +1,9 @@
+
+module JWT
+  module JWK
+    class RSA < KeyBase
+      class PrivateKey
+      end
+    end
+  end
+end
---
+++ lib/jwt/jwk/rsa/public_key.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module JWT
+  module JWK
+    class RSA
+      class PublicKey
+      end
+    end
+  end
+end
