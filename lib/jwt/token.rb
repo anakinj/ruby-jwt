f700dc57b8800af9c959c667dac4106fadc57157
Matteo Pierro <pierromatteo@gmail.com>
2024-10-04 15:23:35 +0200

    remove inheritance

--- lib/jwt/token.rb
+++ lib/jwt/token.rb
@@ -41,9 +41,7 @@ module JWT
     # Returns the decoded header of the JWT token.
     #
     # @return [Hash] the header of the JWT token.
-    def header
-      @header ||= parse_and_decode(encoded_header)
-    end
+    attr_reader :header
 
     # Returns the encoded header of the JWT token.
     #
@@ -55,9 +53,7 @@ module JWT
     # Returns the payload of the JWT token.
     #
     # @return [Hash] the payload of the JWT token.
-    def payload
-      @payload ||= parse_and_decode(encoded_payload)
-    end
+    attr_reader :payload
 
     # Returns the encoded payload of the JWT token.
     #
@@ -70,7 +66,7 @@ module JWT
     #
     # @return [String] the signing input of the JWT token.
     def signing_input
-      [encoded_header, encoded_payload].join('.')
+      @signing_input ||= [encoded_header, encoded_payload].join('.')
     end
 
     # Returns the JWT token as a string.
@@ -150,17 +146,9 @@ module JWT
     #
     # @return [String] the JWT token as a string.
     alias to_s jwt
-
-    private
-
-    def parse_and_decode(segment)
-      JWT::JSON.parse(::JWT::Base64.url_decode(segment))
-    rescue ::JSON::ParserError
-      raise JWT::DecodeError, 'Invalid segment encoding'
-    end
   end
 
-  class EncodedToken < Token
+  class EncodedToken
     attr_reader :jwt
 
     # Initializes a new EncodedToken instance.
@@ -171,8 +159,111 @@ module JWT
 
       @jwt = jwt
       @encoded_header, @encoded_payload, @encoded_signature = jwt.split('.')
+      @signing_input = [encoded_header, encoded_payload].join('.')
+    end
 
-      super(header: nil, payload: nil)
+    # Returns the decoded signature of the JWT token.
+    #
+    # @return [String] the decoded signature of the JWT token.
+    def signature
+      @signature ||= ::JWT::Base64.url_decode(encoded_signature || '')
+    end
+
+    # Returns the encoded signature of the JWT token.
+    #
+    # @return [String] the encoded signature of the JWT token.
+    attr_reader :encoded_signature
+
+    # Returns the decoded header of the JWT token.
+    #
+    # @return [Hash] the header of the JWT token.
+    def header
+      @header ||= parse_and_decode(@encoded_header)
+    end
+
+    # Returns the encoded header of the JWT token.
+    #
+    # @return [String] the encoded header of the JWT token.
+    attr_reader :encoded_header
+
+    # Returns the payload of the JWT token.
+    #
+    # @return [Hash] the payload of the JWT token.
+    def payload
+      @payload ||= parse_and_decode(encoded_payload)
+    end
+
+    # Returns the encoded payload of the JWT token.
+    #
+    # @return [String] the encoded payload of the JWT token.
+    attr_reader :encoded_payload
+
+    # Returns the signing input of the JWT token.
+    #
+    # @return [String] the signing input of the JWT token.
+    attr_reader :signing_input
+
+    # Verifies the claims of the JWT token.
+    #
+    # @param options [Array] the options for verifying the claims.
+    # @return [void]
+    # @raise [JWT::DecodeError] if any claim is invalid.
+    def verify_claims!(*options)
+      Claims.verify!(self, *options)
+    end
+
+    # Checks if the claims of the JWT token are valid.
+    #
+    # @param options [Array] the options for verifying the claims.
+    # @return [Boolean] true if the claims are valid, false otherwise.
+    def valid_claims?(*options)
+      claim_errors(*options).empty?
+    end
+
+    # Returns the errors in the claims of the JWT token.
+    #
+    # @param options [Array] the options for verifying the claims.
+    # @return [Array<JWT::Claims::Error>] the errors in the claims of the JWT token.
+    def claim_errors(*options)
+      Claims.errors(self, *options)
+    end
+
+    # Verifies the signature of the JWT token.
+    #
+    # @param algorithm [String, Array<String>, Object, Array<Object>] the algorithm(s) to use for verification.
+    # @param key [String, Array<String>] the key(s) to use for verification.
+    # @return [void]
+    # @raise [JWT::VerificationError] if the signature verification fails.
+    def verify_signature!(algorithm:, key:)
+      return if valid_signature?(algorithm: algorithm, key: key)
+
+      raise JWT::VerificationError, 'Signature verification failed'
+    end
+
+    # Checks if the signature of the JWT token is valid.
+    #
+    # @param algorithm [String, Array<String>, Object, Array<Object>] the algorithm(s) to use for verification.
+    # @param key [String, Array<String>] the key(s) to use for verification.
+    # @return [Boolean] true if the signature is valid, false otherwise.
+    def valid_signature?(algorithm:, key:)
+      Array(JWA.resolve_and_sort(algorithms: algorithm, preferred_algorithm: header['alg'])).any? do |algo|
+        Array(key).any? do |one_key|
+          algo.verify(data: signing_input, signature: signature, verification_key: one_key)
+        end
+      end
+    end
+
+    # Returns the JWT token as a string.
+    #
+    # @return [String] the JWT token as a string.
+    alias to_s jwt
+
+    private
+
+    def parse_and_decode(segment)
+      JWT::JSON.parse(::JWT::Base64.url_decode(segment))
+    rescue ::JSON::ParserError
+      raise JWT::DecodeError, 'Invalid segment encoding'
     end
   end
 end
